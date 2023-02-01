ac7143a48cf1b2cebed5705ad41eccdf621ffda8
Mark Maksi <mmaksi.dev@gmail.com>
2023-02-01 13:32:58 +0100

    Global css using _app.js and global.css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
         <Script
           src="https://connect.facebook.net/en_US/sdk.js"
           strategy="lazyOnload"
@@ -14,12 +17,12 @@ export default function FirstPost() {
             console.log(`script loaded correctly, window.FB has been populated`)
           }
         />
-      </Head>
 
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
     </>
   );
 }
